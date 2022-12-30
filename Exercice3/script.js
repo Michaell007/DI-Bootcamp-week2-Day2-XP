48a32f283d13a7497e17ab0ab54a8c76b61c1043
MichaelYaps <michaelakichi@gmail.com>
2022-12-30 17:58:50 +0000

    Initial commit

--- Exercice3/script.js
+++ Exercice3/script.js
@@ -1,13 +1,8 @@
-let entry = prompt("Entrer un nombre");
+let value = prompt("Entrer un nombre");
 
-// extraire le dernier element du nombre
-let lastNumber = entry[entry.length-1];
-
-let pairs = [0,2,4,6,8];
-// let impairs = [1,3,5,7,9]
-
-if (pairs.includes( Number(lastNumber) )) {
-    console.log("x is an even number");
+// determinons si value est pair ou impair
+if (Number(value) % 2 == 0) {
+    console.log(`${value} is an even number`);
 } else {
-    console.log("x is an odd number");
+    console.log(`${value} is an odd number`);
 }
